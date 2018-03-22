b3ed03eb240d3f43103c3f104e4af365ccf009ee
zhangjintao <hi.zhangjt@gmail.com>
2018-03-22 11:17:56 +0800

    add src code

---
+++ src/Http.php
@@ -0,0 +1,55 @@
+<?php
+namespace EzHttp;
+
+class Http
+{
+    public $method;
+    public $data;
+    public $uri;
+    public $response;
+
+    /**
+     * 构造http报文
+     * @param $content
+     * @return string
+     */
+    public function httpEncode($content)
+    {
+        $header = '';
+        $header .= "HTTP/1.1 200 OK\r\n";
+        $header .= "Content-Type: text/html;charset=utf-8\r\n";
+        $header .= "\r\n\r\n";
+
+        return $header . $content;
+    }
+
+    /**
+     * 解析http报文
+     * @param $buffer
+     */
+    public function httpDecode($buffer)
+    {
+        list($http_header, $http_body) = explode("\r\n\r\n", $buffer, 2);
+        $header_data = explode("\r\n", $http_header);
+        $request_line = $header_data[0];
+        unset($header_data[0]);
+        $request_header = $header_data;
+        list($method, $uri, $protocol) = explode(' ', $request_line, 3);
+        $this->method = $method;
+        $this->uri = $uri;
+        $this->data = $http_body;
+    }
+
+    /**
+     * 处理请求
+     * @param callable $callback
+     */
+    public function handle(callable $callback)
+    {
+        //TODO: create a routine map, and search method in routine map
+
+        $data = $callback();
+
+        $this->response = $this->httpEncode($data);
+    }
+}
---
+++ src/Worker.php
@@ -0,0 +1,86 @@
+<?php
+namespace EzHttp;
+
+Class Worker
+{
+    public $mainSocket;
+
+    /**
+     * fork worker num
+     * @var int
+     */
+    public $count = 1;
+
+    /**
+     * @var null|string
+     */
+    public $host = '0.0.0.0:80';
+
+    /**
+     * @var string application root dir
+     */
+    public $appRoot = ROOT . '/Application';
+
+    /**
+     * http处理回调
+     * @var callable
+     */
+    public $onMessage;
+
+    public function __construct($host = null)
+    {
+        if ($host) {
+            $this->host = $host;
+        }
+    }
+
+    public function run()
+    {
+        // fork worker
+        $this->forkWorker();
+        // monitor worker
+        $this->monitorWorker();
+    }
+
+    /**
+     * TODO: 频繁的实例化，销毁对象，性能是否有问题？可能会出现OOM问题？能否引入对象池？
+     * fork worker
+     * @throws \Exception
+     */
+    public function forkWorker()
+    {
+        $socket = stream_socket_server("tcp://$this->host", $errno, $errstr);
+        for ($i = 0; $i < $this->count; $i++) {
+            $pid = pcntl_fork();
+            if ($pid > 0) {
+                //parent worker
+            } elseif ($pid == 0) {
+                //child worker
+                while (1) {
+                    $http = new Http();
+                    // accept
+                    $newSocket = @stream_socket_accept($socket, -1, $remote_address);
+                    $buffer = @fread($newSocket, 65536);
+                    if ($buffer) {
+                        $http->httpDecode($buffer);
+                        $http->handle($this->onMessage);
+                        $str = $http->response;
+                        @fwrite($newSocket, $str, strlen($str));
+                    }
+                    @fclose($newSocket);
+                    unset($http);
+                }
+            } else {
+                throw new \Exception('fork worker fail');
+            }
+        }
+    }
+
+    public function monitorWorker()
+    {
+        // 回收结束的子进程
+        while (1) {
+            pcntl_wait($status, WUNTRACED);
+        }
+    }
+}
